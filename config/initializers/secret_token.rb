a47030c6d7990957ba1486e203219c2124711a35
Arunram Kalaiselvan <arun@edengroups.com>
2012-09-12 20:36:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billnemo::Application.config.secret_token = 'dd60e35c3a86eee103c983f20786d95482288b0742a2ac625b9930725d4e87a1a00b5e54092a903d10daffe770ff717e9a1cee7fe667fc071de8d259a55be142'
