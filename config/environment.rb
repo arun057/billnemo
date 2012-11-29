caf4b0568b68c8bdbd32c9c68979c689e76da972
Arunram Kalaiselvan <arun@edengroups.com>
2012-11-29 11:17:17 -0500

    updating stuff

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Billnemo::Application.initialize!
+
+
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+:enable_starttls_auto => true,  
+:address            => 'smtp.gmail.com',
+:port               => 587,
+:tls                  => true,
+:domain             => 'gmail.com', #you can also use google.com
+:authentication     => :plain,
+:user_name          => ENV['billnemo_gmail_login'],
+:password           => ENV['billnemo_gmail_password']
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,21 +36,4 @@ Billnemo::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
-  require 'tlsmail'
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,  
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :tls                  => true,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => ENV['billnemo_gmail_login'],
-    :password           => ENV['billnemo_gmail_password']
-  }
 end
